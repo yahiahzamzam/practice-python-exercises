331e48855e699cf06a4cc3ffee679ced4bb0df1a
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-06-21 18:07:37 +0300

    Replaced input with a newly created function.

--- exc11.py
+++ exc11.py
@@ -1,5 +1,9 @@
+def get_num():
+    return int(input("Enter a number, please."))
+
+
 def prime_indecator():
-    num = int(input("Please enter a number"))
+    num = get_num()
     factors = [divisor for divisor in range(2, num) if num % divisor == 0]
     if factors == []:
         return "Prime"
