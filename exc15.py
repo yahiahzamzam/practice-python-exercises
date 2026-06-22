92676f53c6f338f96d48bfa735f4d2dffe121138
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-06-22 12:04:33 +0300

    Improved and shorter. Tried this idea, but didn't work as I didn't try proporly.

--- exc15.py
+++ exc15.py
@@ -1,7 +1,7 @@
 def reverse_word():
     sent = input("Please enter your sentence")
     words = sent.split()
-    rvs = [words[-1-num] for num in range(0, len(words))]
+    rvs = words[::-1]
     rvs_sent = " ".join(rvs)
     return rvs_sent
 
