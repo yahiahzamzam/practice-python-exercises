ee63d72a2e8dc239bcb4b9d3fd45006c9e8ccaae
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-07-17 08:01:49 +0300

    Test case successfull. List input unseccessfull

--- exc20.py
+++ exc20.py
@@ -1,5 +1,3 @@
-# ordered_lst = input("Please enter your list")
-
 a = [1, 3, 5, 30, 42, 43, 500]
 
 num = int(input("enter a number to be found"))
@@ -10,6 +8,7 @@ def binary_search(arr, target):
     high = len(arr) - 1
     while True:
         mid = (low + high) // 2
+        arr[mid] = int(arr[mid])
         if arr[mid] == target:
             return mid
         elif arr[mid] > target:
@@ -18,3 +17,10 @@ def binary_search(arr, target):
             low = mid + 1
         else:
             return None
+
+
+print(f"Test-enter 5_expect 2: {binary_search(a, num)}")
+num = int(input("enter a number to be found"))
+ordered_lst: list[int]
+ordered_lst = list(input("Please enter your list"))
+print(binary_search(ordered_lst, num))
