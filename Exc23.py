85bc7e925c771a3f2446009e57385cc406190223
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-08-04 01:46:35 +0300

    Storred the numbers in the second file in a list

--- Exc23.py
+++ Exc23.py
@@ -1,2 +1,5 @@
 with open(r"C:\Coding stuff\prime.txt", "r", encoding="utf-8") as p:
     primes = [int(line.strip()) for line in p]
+
+with open(r"C:\Coding stuff\happy.txt", "r", encoding="utf-8") as h:
+    happies = [int(line.strip()) for line in h]
