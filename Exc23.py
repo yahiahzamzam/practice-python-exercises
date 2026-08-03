a60d85f0bd6e8d3aa85c3977f2340b07077fc857
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-08-04 01:48:49 +0300

    algorithm to find overlapping numbers is done

--- Exc23.py
+++ Exc23.py
@@ -3,3 +3,11 @@ with open(r"C:\Coding stuff\prime.txt", "r", encoding="utf-8") as p:
 
 with open(r"C:\Coding stuff\happy.txt", "r", encoding="utf-8") as h:
     happies = [int(line.strip()) for line in h]
+
+overlapping = []
+for num1 in primes:
+    for num2 in happies:
+        if num1 == num2:
+            overlapping.append(num2)
+
+print(overlapping)
