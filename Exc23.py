0edaec636de36d89f0131be882f67f8a8f24b558
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-08-04 01:54:04 +0300

    Used sets rather than list for better efficiency

--- Exc23.py
+++ Exc23.py
@@ -1,13 +1,9 @@
 with open(r"C:\Coding stuff\prime.txt", "r", encoding="utf-8") as p:
-    primes = [int(line.strip()) for line in p]
+    primes = {int(line.strip()) for line in p}
 
 with open(r"C:\Coding stuff\happy.txt", "r", encoding="utf-8") as h:
-    happies = [int(line.strip()) for line in h]
+    happies = {int(line.strip()) for line in h}
 
-overlapping = []
-for num1 in primes:
-    for num2 in happies:
-        if num1 == num2:
-            overlapping.append(num2)
+overlapping = sorted(list(primes & happies))
 
 print(overlapping)
