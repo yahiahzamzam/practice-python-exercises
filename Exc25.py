644ddaf2c115f39bf09f930d8120c5ae0af53418
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-08-07 22:54:35 +0300

    Removed unnecessary input. Fixed bug of high and low new assignment.

--- Exc25.py
+++ Exc25.py
@@ -1,20 +1,19 @@
-def binary_search(arr_len, num):
+def binary_search(arr_len):
     low = 0
     high = arr_len - 1
     while low <= high:
         mid = (low + high) // 2
         guess = input(
-            f"Is {num} in index {mid}?\nYes: Y\nToo high: H\nToo low: L\n")
+            f"Is your number in index {mid}?\nYes: Y\nToo high: H\nToo low: L\n")
         if guess.upper() == "Y":
             return mid
-        elif guess.upper == "H":
-            high = mid
+        elif guess.upper() == "H":
+            high = mid - 1
         elif guess.upper() == "L":
-            low = mid
+            low = mid + 1
     return None
 
 
-num_to_be_found = int(input("What is the number I am looking for?"))
 arr_length = int(input("What is the length of the array?"))
 
-print(binary_search(arr_length, num_to_be_found))
+print(binary_search(arr_length))
