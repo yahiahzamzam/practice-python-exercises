2d5bb0c2b7129a7c129685720cfa8fc5a703f84d
Yahia Zamzam <yahiazamzam2@gmail.com>
2026-08-07 22:58:34 +0300

    Added the number of guesses feature

--- Exc25.py
+++ Exc25.py
@@ -1,12 +1,14 @@
 def binary_search(arr_len):
     low = 0
     high = arr_len - 1
+    guesses = 0
     while low <= high:
+        guesses += 1
         mid = (low + high) // 2
         guess = input(
             f"Is your number in index {mid}?\nYes: Y\nToo high: H\nToo low: L\n")
         if guess.upper() == "Y":
-            return mid
+            return mid, guesses
         elif guess.upper() == "H":
             high = mid - 1
         elif guess.upper() == "L":
